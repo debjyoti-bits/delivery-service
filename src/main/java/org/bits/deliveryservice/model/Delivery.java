a3040913a522a9f235040baa4ab88827d4c3d072
debjpaul-bits <Debu@1630>
2024-11-17 17:33:06 +0100

    fixed model

--- src/main/java/org/bits/deliveryservice/model/Delivery.java
+++ src/main/java/org/bits/deliveryservice/model/Delivery.java
@@ -16,7 +16,7 @@ import java.sql.Timestamp;
 public class Delivery {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long orderId;
